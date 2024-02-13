e218c03a7250cfef313163baef64b20bfed32c15
Henock-Lite <litehenock@gmail.com>
2024-02-13 06:24:33 +0300

    js mousse

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const black = document.querySelector(".black");
 const green = document.querySelector(".green");
 const orange = document.querySelector(".orange");
+
 window.addEventListener("mousemove", (e) => {
-  black.style.top = e.pageY + "px";
-  black.style.left = e.pageX + "px";
-});
-window.addEventListener("mousemove", (e) => {
-  green.style.top = e.pageY + "px";
-  green.style.left = e.pageX + "px";
-});
-window.addEventListener("mousemove", (e) => {
-  orange.style.top = e.pageY + "px";
-  orange.style.left = e.pageX + "px";
+  const top = e.pageY + "px";
+  const left = e.pageX + "px";
+
+  black.style.top = top;
+  black.style.left = left;
+
+  green.style.top = top;
+  green.style.left = left;
+
+  orange.style.top = top;
+  orange.style.left = left;
 });
